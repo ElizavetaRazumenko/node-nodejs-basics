a70c4f402219ce4379b01a72faf162a5daa5f539
Elizaveta Razumenko <razumenko99@mail.ru>
2024-01-24 13:27:51 +0300

    feat: add functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
+
+const customErrMessage = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.access('files', constants.F_OK);
+        const files = await fs.readdir('files');
+        
+        try {
+            await fs.access('files_copy', constants.F_OK);
+            throw Error(customErrMessage);
+        } catch (e) {
+            if (e.message !== customErrMessage) {
+                fs.mkdir('files_copy');
+                files.forEach(async (file) => {
+                    const content = await fs.readFile(`files/${file}`, 'utf8');
+                    await fs.writeFile(`files_copy/${file}`, content);
+                });
+            } else throw Error(customErrMessage);
+        }
+    } catch (e) {
+        throw Error(customErrMessage);
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
+
+const text = 'I am fresh and young';
+const customErrMessage = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.access('files/fresh.txt', constants.F_OK);
+        throw Error(customErrMessage);
+    } catch (e) {
+        if (e.message !== customErrMessage) {
+            await fs.writeFile('files/fresh.txt', text);
+        } else throw Error(customErrMessage);
+    }
 };
 
 await create();
