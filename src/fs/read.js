5184459795f7803aff5e9638d216cbb2bdadc2ff
Elizaveta Razumenko <razumenko99@mail.ru>
2024-01-24 14:13:38 +0300

    feat: realize fs functions

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
+
+const customErrMessage = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        await fs.access('files/fileToRead.txt', constants.F_OK);
+        const content = await fs.readFile('files/fileToRead.txt', 'utf8');
+        console.log(content);
+    } catch (e) {
+        throw Error(customErrMessage);
+    }
+
 };
 
 await read();
