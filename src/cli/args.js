6ef780e144d1f2140a1bf449d73fa288689c07b3
ElizavetaRazumenko <razumenko99@mail.ru>
2024-01-25 21:59:37 +0300

    feat: fix cli bugs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,13 +1,16 @@
 const parseArgs = () => {
-    const args = process.argv;
+  const args = process.argv.slice(2);
 
-    const indexes = args.map((item, index) => {
-        if (item.startsWith('--')) {
-            return index;
-        }
-    }).filter((index) => index);
-
-    indexes.forEach((index) => console.log(`${args[index]} is ${args[index + 1]}`));
+  const indexes = args
+    .map((item, index) => {
+      if (item.startsWith("--")) {
+        return index;
+      }
+    })
+    .filter((index) => index);
+    
+    const resultArr = indexes.map((index) => `${args[index].slice(2)} is ${args[index + 1]}`);
+    console.log(resultArr.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,19 @@
+// let's create a couple of variables to test the function's operation
+process.env.RSS_VAR1 = "value1";
+process.env.RSS_VAR2 = "value2";
+process.env.RSS_VAR3 = "value3";
+
 const parseEnv = () => {
-    const envVars = process.env;
-    for (const envKey of Object.keys(envVars)) {
-        if (envKey.startsWith('RSS_')) {
-            console.log(`${envKey}=${envVars[envKey]}`);
-        }
+  const envVars = process.env;
+  const result = [];
+
+  for (const envKey of Object.keys(envVars)) {
+    if (envKey.startsWith("RSS_")) {
+      result.push(`${envKey}=${envVars[envKey]}`);
     }
+  }
+
+  console.log(result.join("; "));
 };
 
 parseEnv();
